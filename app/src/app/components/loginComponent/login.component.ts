336915d262945542128ab33775488660048bbc76
Ramesh <rameshbg11@gmail.com>
2018-08-16 17:28:36 +0530

    final touch

---
+++ app/src/app/components/loginComponent/login.component.ts
@@ -0,0 +1,136 @@
+/*DEFAULT GENERATED TEMPLATE. DO NOT CHANGE SELECTOR TEMPLATE_URL AND CLASS NAME*/
+import { Component, OnInit } from '@angular/core'
+import { ModelMethods } from '../../lib/model.methods';
+import { NDataModelService } from 'neutrinos-seed-services';
+import { NLoginService } from 'neutrinos-seed-services';
+import {Router} from '@angular/router'
+import {NSessionStorageService} from 'neutrinos-seed-services';
+
+/**
+* Model import Example :
+* import { HERO } from '../models/hero.model';
+*/
+
+/**
+ * Service import Example :
+ * import { HeroService } from '../services/hero/hero.service';
+ */
+
+@Component({
+    selector: 'bh-login',
+    templateUrl: './login.template.html'
+})
+
+export class loginComponent implements OnInit {
+    dm: ModelMethods;
+    username;
+remember;
+password;
+
+
+    constructor(private bdms: NDataModelService, private loginservice: NLoginService,private router:Router,private local:NSessionStorageService) {
+       let login = this.loginservice.isLoggedIn();
+       console.log(login);
+       if(login){
+           this.router.navigate(['/home']);
+       }
+       
+       
+        this.dm = new ModelMethods(bdms);
+    }
+
+    ngOnInit() {
+
+    }
+     submit(){
+         
+        this.loginservice.login(this.username,this.password).subscribe(data=>{
+            console.log(data);
+            if(data){
+                this.router.navigate(['/home']);
+            }
+        })
+
+    }
+
+    get(dataModelName, filter ?, keys ?, sort ?, pagenumber ?, pagesize ?) {
+        this.dm.get(dataModelName, this, filter, keys, sort, pagenumber, pagesize,
+            result => {
+                // On Success code here
+            },
+            error => {
+                // Handle errors here
+            });
+    }
+
+    getById(dataModelName, dataModelId) {
+        this.dm.getById(dataModelName, dataModelId,
+            result => {
+                // On Success code here
+            },
+            error => {
+                // Handle errors here
+            })
+    }
+
+    put(dataModelName, dataModelObject) {
+        this.dm.put(dataModelName, dataModelObject,
+            result => {
+                // On Success code here
+            }, error => {
+                // Handle errors here
+            })
+    }
+
+    validatePut(formObj, dataModelName, dataModelObject) {
+        this.dm.validatePut(formObj, dataModelName, dataModelObject,
+            result => {
+                // On Success code here
+            }, error => {
+                // Handle errors here
+            })
+    }
+
+    update(dataModelName, update, filter, options) {
+        const updateObject = {
+            update: update,
+            filter: filter,
+            options: options
+        };
+        this.dm.update(dataModelName, updateObject,
+            result => {
+                //  On Success code here
+            }, error => {
+                // Handle errors here
+            })
+    }
+
+    delete (dataModelName, filter) {
+        this.dm.delete(dataModelName, filter,
+            result => {
+                // On Success code here
+            }, error => {
+                // Handle errors here
+            })
+    }
+
+    deleteById(dataModelName, dataModelId) {
+        this.dm.deleteById(dataModelName, dataModelId,
+            result => {
+                // On Success code here
+            }, error => {
+                // Handle errors here
+            })
+    }
+
+    updateById(dataModelName, dataModelId, dataModelObj) {
+        this.dm.updateById(dataModelName, dataModelId, dataModelObj,
+            result => {
+                // On Success code here
+            }, error => {
+                // Handle errors here
+            })
+    }
+
+
+}
